d41108ef39f4c10bb035d4397bd6d49558fcec87
Toshiyuki Ogawa <toshi@oc-soft.net>
2020-07-21 05:29:12 +0900

    Updated webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,49 @@
 const path = require('path');
-const HtmlWebPack = require('html-webpack-plugin');
+const HtmlWebpack = require('html-webpack-plugin');
 const HtmlCdn = require('webpack-cdn-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const config = {
   mode: 'development',
   entry: './src/index.js',
+  module: {
+    rules:[ 
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      }
+    ]  
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle-[hash].js'
   },
   externals: [
-    'lodash'  
+    function() {
+      const arg0 = arguments[0];
+      let context = undefined;
+      let request = undefined;
+      let cb = undefined;
+      if (arg0.request) {
+        context = arg0.context;
+        request = arg0.request;
+        cb = arguments[1];
+      } else {
+        context = arguments[0];
+        request = arguments[1]; 
+        cb = arguments[2];
+      }
+      if ('lodash' == request) {
+        cb(undefined, '_', 'window');
+      } else {
+        cb();
+      }
+    }
   ], 
   plugins: [
-    new HtmlWebPack(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpack(),
     new HtmlCdn({
       modules: [
         { 
